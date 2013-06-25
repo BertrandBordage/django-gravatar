b7b585fe71220e7e55d8ad15733730f4dd585d06
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-06-25 19:34:23 +0200

    Code styling in template tags.

--- django_gravatar/templatetags/gravatar.py
+++ django_gravatar/templatetags/gravatar.py
@@ -6,9 +6,13 @@ from django_gravatar.helpers import get_gravatar_url, has_gravatar as has_gravat
 # Get template.Library instance
 register = template.Library()
 
+
+@register.filter
 def has_gravatar(user_or_email):
     return has_gravatar_helper(user_or_email)
 
+
+@register.simple_tag
 def gravatar_url(user_or_email, size=GRAVATAR_DEFAULT_SIZE):
     """ Builds a gravatar url from an user or email """
     if hasattr(user_or_email, 'email'):
@@ -21,6 +25,8 @@ def gravatar_url(user_or_email, size=GRAVATAR_DEFAULT_SIZE):
     except:
         return ''
 
+
+@register.simple_tag
 def gravatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE, alt_text='', css_class='gravatar'):
     """ Builds an gravatar <img> tag from an user or email """
     if hasattr(user_or_email, 'email'):
@@ -35,7 +41,3 @@ def gravatar(user_or_email, size=GRAVATAR_DEFAULT_SIZE, alt_text='', css_class='
 
     return '<img class="{css_class}" src="{src}" width="{width}" height="{height}" alt="{alt}" />'.format(\
         css_class=css_class, src=url, width=size, height=size, alt=alt_text)
-
-register.filter(has_gravatar)
-register.simple_tag(gravatar_url)
-register.simple_tag(gravatar)
